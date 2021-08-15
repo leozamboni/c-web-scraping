3c7ffcd2d44d0b168080401d5069136ef0b71674
leonardoDomper <leonardonunes169@gmail.com>
2021-08-15 02:37:46 -0300

    update

--- scraping.c
+++ scraping.c
@@ -1,6 +1,6 @@
 /*
  * Web Scraping C 
- * Copyright © 2021 Leonardo Zamboni 🌎 leozamboni.dev
+ * Copyright © 2021 Leonardo Zamboni
  *
  * this program is free software: you can redistribute it and/or modify
  * it under the terms of the gnu general public license as published by
